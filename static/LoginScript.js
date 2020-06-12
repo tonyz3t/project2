dbe4efc253e12608a9d96db89695ea7c994ee584
tonyz3t <dhanjal.n3@gmail.com>
2020-06-12 14:23:06 -0700

    added templates and static folder

---
+++ static/LoginScript.js
@@ -0,0 +1,9 @@
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    // check to see if the displayname is saved in local storage
+    if(!localStorage.getItem('username')) {
+        document.querySelector('#username').innerHTML
+    }
+})
